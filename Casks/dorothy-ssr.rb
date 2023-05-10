aab6416ab8564700337e57561c429b7170d4addf
Li Jin <dragon-fly@qq.com>
2023-05-10 15:18:53 +0800

    fix cask issues.

--- Casks/dorothy-ssr.rb
+++ Casks/dorothy-ssr.rb
@@ -1,8 +1,8 @@
 cask "dorothy-ssr" do
   version "1.0.25"
-  sha256 ":no_check"
+  sha256 :no_check
 
-  url "https://github.com/pigpigyyy/Dorothy-SSR/releases/download/v#{version}/dorothy-ssr-v#{version}-macos-universal.zip"
+  url "https://github.com/pigpigyyy/Dorothy-SSR/releases/download/v#{version}/dorothy-ssr-v#{version}-macos-universal.zip", , verified: "github.com/pigpigyyy/Dorothy-SSR/"
   name "Dorothy SSR"
   desc "A game engine for rapid game development"
   homepage "https://www.dorothy-ssr.net"
