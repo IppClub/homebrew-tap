d9e7ace851523f28d0f081b621d21d99dddbc2d2
Li Jin <dragon-fly@qq.com>
2023-05-10 15:05:52 +0800

    initial commit.

---
+++ Casks/dorothy-ssr.rb
@@ -0,0 +1,13 @@
+cask "dorothy-ssr" do
+  version "1.0.25"
+  sha256 ":no_check"
+
+  url "https://github.com/pigpigyyy/Dorothy-SSR/releases/download/v#{version}/dorothy-ssr-#{version}-macos-universal.zip"
+  name "Dorothy SSR"
+  desc "A game engine for rapid game development"
+  homepage "https://www.dorothy-ssr.net"
+
+  depends_on macos: ">= :big_sur"
+
+  app "Dorothy SSR.app"
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# homebrew-dorothy-ssr
+
+Homebrew tap for PlayCover and related tools.
+
+## Casks
+```
+dorothy-ssr
+```
+
+## Installing
+
+```
+brew tap pigpigyyy/dorothy-ssr
+brew install --cask dorothy-ssr
+```
+
+## Uninstalling
+
+```
+brew uninstall --cask dorothy-ssr
+brew untap pigpigyyy/dorothy-ssr
+```
