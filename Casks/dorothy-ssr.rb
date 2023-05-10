edc6f831c77185290fe786ce42ff1aee56c21434
Li Jin <dragon-fly@qq.com>
2023-05-10 15:19:28 +0800

    more typo.

--- Casks/dorothy-ssr.rb
+++ Casks/dorothy-ssr.rb
@@ -2,7 +2,7 @@ cask "dorothy-ssr" do
   version "1.0.25"
   sha256 :no_check
 
-  url "https://github.com/pigpigyyy/Dorothy-SSR/releases/download/v#{version}/dorothy-ssr-v#{version}-macos-universal.zip", , verified: "github.com/pigpigyyy/Dorothy-SSR/"
+  url "https://github.com/pigpigyyy/Dorothy-SSR/releases/download/v#{version}/dorothy-ssr-v#{version}-macos-universal.zip", verified: "github.com/pigpigyyy/Dorothy-SSR/"
   name "Dorothy SSR"
   desc "A game engine for rapid game development"
   homepage "https://www.dorothy-ssr.net"
