5899932d7ca9a515b598fa37f3c1ef95ed0e65a2
Li Jin <dragon-fly@qq.com>
2023-06-01 19:53:20 +0800

    update.

--- Casks/dorothy-ssr.rb
+++ Casks/dorothy-ssr.rb
@@ -1,5 +1,5 @@
 cask "dorothy-ssr" do
-  version "1.0.28"
+  version "1.0.29"
   sha256 :no_check
 
   url "https://github.com/pigpigyyy/Dorothy-SSR/releases/download/v#{version}/dorothy-ssr-v#{version}-macos-universal.zip", verified: "github.com/pigpigyyy/Dorothy-SSR/"
