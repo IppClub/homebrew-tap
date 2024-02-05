5a4972b4c2fac0f143e96cd44913b5f21a1cca16
Li Jin <dragon-fly@qq.com>
2024-02-05 16:15:06 +0800

    Update dora-ssr.rb

--- Casks/dora-ssr.rb
+++ Casks/dora-ssr.rb
@@ -2,7 +2,7 @@ cask "dora-ssr" do
   version "1.3.6"
   sha256 :no_check
 
-  url "https://github.com/ippclub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip", verified: "github.com/ippclub/Dora-SSR/"
+  url "https://github.com/IppClub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip", verified: "github.com/IppClub/Dora-SSR/"
   name "Dora SSR"
   desc "Game engine for rapid game development"
   homepage "https://www.dora-ssr.net/"
