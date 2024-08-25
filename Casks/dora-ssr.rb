6058b285906223144df4db6e6f1794e439bcb265
Li Jin <dragon-fly@qq.com>
2024-08-25 17:01:02 +0800

    Update dora-ssr.rb

--- Casks/dora-ssr.rb
+++ Casks/dora-ssr.rb
@@ -1,5 +1,5 @@
 cask "dora-ssr" do
-  version "1.5.11"
+  version "1.5.12"
   sha256 :no_check
 
   url "https://github.com/IppClub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip", verified: "github.com/IppClub/Dora-SSR/"
