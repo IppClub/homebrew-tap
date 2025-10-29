bca99726352ad9d1527ae8b3adf68f8145112573
Li Jin <dragon-fly@qq.com>
2025-10-29 15:28:04 +0800

    Update Dora SSR to version 1.7.2

--- Casks/dora-ssr.rb
+++ Casks/dora-ssr.rb
@@ -1,6 +1,6 @@
 cask "dora-ssr" do
-  version "1.7.1"
-  sha256 "0fc063d7e23b0225ca74cbd977b585a727932684ee849a5d1d13e7b08adf27e1"
+  version "1.7.2"
+  sha256 "42620c1aaa9f6efffca446b211e84e24c42b24a6be8025a5180b1c7126eae855"
 
   url "https://github.com/IppClub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip",
       verified: "github.com/IppClub/Dora-SSR/"
