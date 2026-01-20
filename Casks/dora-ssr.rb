428aa5226f2aad76bec24e413a6bbde10d6d02b2
Li Jin <dragon-fly@qq.com>
2026-01-20 09:00:22 +0800

    Update SHA256 checksum for dora-ssr
    Updated the SHA256 checksum for version 1.7.5.

--- Casks/dora-ssr.rb
+++ Casks/dora-ssr.rb
@@ -1,6 +1,6 @@
 cask "dora-ssr" do
   version "1.7.5"
-  sha256 "f5a6638b048f9dc81ea722843d1ea872b2095828f37363ac023f7d10e9ce0816"
+  sha256 "2a1fdb26fb0efddd87642350d72c6b37c763306c78e27cec10001b1c1e3259ba"
 
   url "https://github.com/IppClub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip",
       verified: "github.com/IppClub/Dora-SSR/"
