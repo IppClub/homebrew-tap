986aba8e4e4ed4d92b9f10fb1c7f818d2d06bdb4
Li Jin <dragon-fly@qq.com>
2026-04-02 14:27:09 +0800

    Update sha256 checksum for Dora SSR

--- Casks/dora-ssr.rb
+++ Casks/dora-ssr.rb
@@ -1,6 +1,6 @@
 cask "dora-ssr" do
   version "1.7.6"
-  sha256 "3808216d5d83359b6b2c7cd0e8eb80db39d1792c63a9f83934a9f5d733e1986d"
+  sha256 "a9aad8eb929cc6cc2121b2751a7f84b186cf82fcea86129d59554c10fb1fc86b"
 
   url "https://github.com/IppClub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip",
       verified: "github.com/IppClub/Dora-SSR/"
