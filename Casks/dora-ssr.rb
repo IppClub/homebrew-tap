f0643a6b4839caa214589fd75555d886acec5200
Li Jin <dragon-fly@qq.com>
2026-09-02 22:10:11 +0800

    Fix cask audit by removing deprecated verified parameter

--- Casks/dora-ssr.rb
+++ Casks/dora-ssr.rb
@@ -2,8 +2,7 @@ cask "dora-ssr" do
   version "1.9.2"
   sha256 "3741d14f73a37bf027c7b07c14170a7650357e488923d806ff3f7a916ea0b76f"
 
-  url "https://github.com/IppClub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip",
-      verified: "github.com/IppClub/Dora-SSR/"
+  url "https://github.com/IppClub/Dora-SSR/releases/download/v#{version}/dora-ssr-v#{version}-macos-universal.zip"
   name "Dora SSR"
   desc "Game engine for rapid game development"
   homepage "https://www.dora-ssr.net/"
