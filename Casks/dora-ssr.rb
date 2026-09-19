ebfe3d433177da60a4eceddeeba84e7cb67ac264
Li Jin <dragon-fly@qq.com>
2026-09-19 11:05:47 +0800

    Fix redundant macOS dependency

--- Casks/dora-ssr.rb
+++ Casks/dora-ssr.rb
@@ -7,7 +7,7 @@ cask "dora-ssr" do
   desc "Game engine for rapid game development"
   homepage "https://www.dora-ssr.net/"
 
-  depends_on macos: :big_sur
+  depends_on :macos
 
   app "Dora.app", target: "Dora SSR.app"
 end
